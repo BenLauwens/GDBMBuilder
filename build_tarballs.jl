929bf3dab861c5b98656deaf1b03867e45fc326b
Ben Lauwens <ben.lauwens@gmail.com>
2018-09-17 20:47:13 +0200

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -19,18 +19,11 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd gdbm-1.18/
-if [ $target = "i686-w64-mingw32" ]; then
+if [ $target = "i686-w64-mingw32" ] || [ $target = "x86_64-w64-mingw32" ]; then
 patch -Np1 -i ../*gdbm-1.15-win32.patch
-touch configure.ac
-./configure --prefix=$prefix --host=$target --without-readline --disable-dependency-tracking --disable-silent-rules
-elif [ $target = "x86_64-w64-mingw32" ]; then
-patch -Np1 -i ../*gdbm-1.15-win32.patch
-touch configure.ac
-./configure --prefix=$prefix --host=$target --without-readline --disable-dependency-tracking --disable-silent-rules
-touch Makefile.am
-else
-./configure --prefix=$prefix --host=$target --without-readline --disable-dependency-tracking --disable-silent-rules
+autoreconf -f
 fi
+./configure --prefix=$prefix --host=$target --without-readline --disable-dependency-tracking --disable-silent-rules
 make
 make install
 exit
@@ -51,8 +44,8 @@ platforms = [
     Linux(:armv7l, :musl, :eabihf),
     MacOS(:x86_64),
     FreeBSD(:x86_64),
-#    Windows(:i686),
-#    Windows(:x86_64)
+    Windows(:i686),
+    Windows(:x86_64)
 ]
 
 # The products that we will ensure are always built
